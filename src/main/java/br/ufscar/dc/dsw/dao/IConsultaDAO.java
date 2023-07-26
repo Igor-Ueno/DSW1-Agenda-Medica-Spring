3896f9c9c7718ff579cdfa701a4d7fba60957b8b
Igor-Ueno <123198443+Igor-Ueno@users.noreply.github.com>
2023-07-26 09:20:15 -0300

    Add files via upload

---
+++ src/main/java/br/ufscar/dc/dsw/dao/IConsultaDAO.java
@@ -0,0 +1,27 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+// import br.ufscar.dc.dsw.domain.Usuarios;
+import br.ufscar.dc.dsw.domain.Consulta;
+
+@SuppressWarnings("unchecked")
+public interface IConsultaDAO extends CrudRepository<Consulta, Long>{
+    
+    Consulta findById(long id);
+	
+	Consulta findByCPF(String CPF);
+
+    Consulta findByCRM(String CRM);
+
+	List<Consulta> findAll();
+	
+	Consulta save(Consulta consulta);
+
+	void deleteById(Long id);
+
+    void deleteByConsulta(String Consulta);
+
+}
